a3e7fd463d3da19c4dece760fb74ccd93e526823
Mirbek <mirbek_karimberdiev@mail.ru>
2023-05-11 16:28:53 +0300

    Stub and tests for add

--- src/main/java/PhoneBook.java
+++ src/main/java/PhoneBook.java
@@ -3,4 +3,14 @@
 public class PhoneBook {
 
 
- 
+    private Map<String, String> map = null;
+
+    public int add(String name, String phoneNumber) {
+        if (map == null) {
+            map = new HashMap<>();
+            map.put(name, phoneNumber);
+        }
+
+        map.put(name, phoneNumber);
+        return map.size();
+    }
--- src/test/java/PhoneBookTest.java
+++ src/test/java/PhoneBookTest.java
@@ -3,3 +3,13 @@ public class PhoneBookTest {
 
     PhoneBook phoneBook = new PhoneBook();
 
+    @Test
+    void testAdd() {
+
+
+        int result = phoneBook.add("Братан", "8-999-134-22-11");
+
+        int expected = 1;
+
+        assertEquals(expected, result);
+    }
