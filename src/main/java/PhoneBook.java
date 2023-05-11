45191fc16543fc54b7198153fe010234da7ed159
Mirbek <mirbek_karimberdiev@mail.ru>
2023-05-11 16:13:57 +0300

    Initial commit

--- src/main/java/PhoneBook.java
+++ src/main/java/PhoneBook.java
@@ -1,40 +1,6 @@
-import java.util.*;
-import java.util.stream.Collectors;
+
 
 public class PhoneBook {
 
 
-    private Map<String, String> map = null;
-
-    public int add(String name, String phoneNumber) {
-        if (map == null) {
-            map = new HashMap<>();
-            map.put(name, phoneNumber);
-        }
-
-        map.put(name, phoneNumber);
-        return map.size();
-    }
-    public String findByNumber(String value) {
-
-        Optional<String> result = map.entrySet()
-                .stream()
-                .filter(entry -> value.equals(entry.getValue()))
-                .map(Map.Entry::getKey)
-                .findFirst();
-
-        return result.get();
-    }
-
-    public String findByName(String key){
-        return map.get(key);
-    }
-
-    public ArrayList<String> printAllNames(){
-        Set<String> keys = map.keySet();
-        return (ArrayList<String>) new ArrayList<>(keys).stream()
-                .sorted()
-                .collect(Collectors.toList());
-    }
-
-}
+ 
